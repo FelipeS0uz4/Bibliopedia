b3856d31373bca2747ebf8e5f7bfcc2499b940f4
Felipe Souza Panichi <141967123+FelipeS0uz4@users.noreply.github.com>
2024-09-19 19:01:36 -0300

    Delete Teste_importação directory

--- Teste_importação/main.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from livro import *
-
-
-blueprint = Blueprint('blueprint',__name__)
-blueprint.route('/livro', methods=['GET'])(TodosLivros)
-blueprint.route('/livro', methods=['POST'])(SalveLivro)

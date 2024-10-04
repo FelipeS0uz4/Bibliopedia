376c10e727dbdd07e0326fe3ab9f793424f0b942
Felipe Souza Panichi <141967123+FelipeS0uz4@users.noreply.github.com>
2024-10-04 09:27:13 -0300

    Delete Bibliopedia/Teste_importação directory

--- Bibliopedia/Teste_importação/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from main import blueprint
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-app.register_blueprint(blueprint)
-app.run(host='127.0.0.1', port=5500, debug=True)

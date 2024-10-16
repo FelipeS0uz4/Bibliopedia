053ff8431e324d524514d70069eeef8c10be81e4
Felipe Souza Panichi <141967123+FelipeS0uz4@users.noreply.github.com>
2024-10-16 14:01:14 -0300

    Add files via upload

---
+++ Bibliopedia/teste_classe_foto/main.py
@@ -0,0 +1,34 @@
+from flask import Blueprint
+from livroController import *
+
+
+blueprint = Blueprint('blueprint',__name__)
+
+##livros
+
+blueprint.route('/livros', methods=['GET'])(TodosLivros)
+blueprint.route('/livro', methods=['POST'])(SalveLivro)
+#blueprint.route('/livro/<int:id>', methods=['DELETE'])(removerUmLivro)
+
+##Usuarios
+
+blueprint.route('/usuarios', methods=['GET'])(listarTodosUsuario)
+blueprint.route('/usuario', methods=['POST'])(salvarUsuario)
+blueprint.route('/usuario/<int:id>', methods=['GET'])(listarApenasUmUsuario)
+blueprint.route('/usuario/<int:id>', methods=['PUT'])(atualizarUmUsuario)
+blueprint.route('/usuario/<int:id>', methods=['DELETE'])(removerUmUsuario)
+blueprint.route('/login', methods=['GET','POST'])(login)
+blueprint.route('/logout',methods=['POST'])(logout)
+
+
+
+
+##Comentario
+blueprint.route('/comentarios/<idLivro>', methods=['GET'])(Vercomentarios)
+blueprint.route('/comentario', methods=['POST'])(salvarComentario)
+
+
+##Rating
+blueprint.route('/rating-media/<idLivro>',methods=['GET'])(notaLivro)
+
+
